001d1d340597abe29387ba4d270c53474fa7bb17
Olga Fomin <olga.fomin26@gmail.com>
2019-07-31 17:25:46 -0700

    Let Board handle Square click

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import Square from './Square';
 
+
 class Board extends React.Component {
-    renderSquare(i, value) {
-        return <Square value={i}/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            isXNext: true
+        }
+    }
+
+    handleClick(i) {
+        console.log('Button was clicked!');
+
+        const squaresCopy = this.state.squares.slice(0);
+        squaresCopy[i] = this.state.isXNext ? 'X' : '0';
+        this.setState({ squares: squaresCopy, isXNext: !this.state.isXNext});
+    }
+
+    renderSquare(i) {
+        // pass its current value to each Square
+        // 'X', 'O', or null for empty squares
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        )
     }
 
     render() {
@@ -11,7 +35,7 @@ class Board extends React.Component {
 
         return (
             <>
-                <div className="status mb3 fw5">{status}</div>
+                <div className="status mb3 fw5 f3">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 function Square(props) {
-  return (
-    <button 
-        className="square pa2 ph3 bg-white ba b--light-gray">{props.value}
-    </button>
-  );
+    return (
+        <button 
+            className="square bg-white ba b--light-gray f4"
+            onClick={props.onClick}>{props.value}
+        </button>
+    );
 }
 
 export default Square;
