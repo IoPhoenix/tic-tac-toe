c9bf9f77c14327187c28fcbd40f8376e13e1e1ba
Olga Fomin <olga.fomin26@gmail.com>
2019-08-04 12:24:45 -0700

    Updates

---
+++ precache-manifest.ff199308314477e1309d5b802e42a803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80903d6563b74180578edb8a1c708f2f",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "096459acccba24dd56c5",
+    "url": "/tic-tac-toe/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "75f093edd7046d66acd9",
+    "url": "/tic-tac-toe/static/css/main.0552df91.chunk.css"
+  },
+  {
+    "revision": "096459acccba24dd56c5",
+    "url": "/tic-tac-toe/static/js/2.765cdff8.chunk.js"
+  },
+  {
+    "revision": "75f093edd7046d66acd9",
+    "url": "/tic-tac-toe/static/js/main.31c48aff.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
